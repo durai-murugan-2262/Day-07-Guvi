cba7b418eb93521449b2a88ae14b12ebaae78773
Hacker of havoc <84559514+hackerofhavoc@users.noreply.github.com>
2022-03-22 13:11:03 +0530

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(name, age, email, num) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.num = num;
+  }
+}
+
+const person = new Person("DM", 20, "havocdurai67@gmail.com", 9876543210);
